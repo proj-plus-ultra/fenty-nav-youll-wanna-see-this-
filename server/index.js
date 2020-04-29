5377dfcc404a96ac1c7b8595dc049d565064e0dd
Alex Benko <benkoalex521@gmail.com>
2020-04-29 11:21:13 -0700

    Server is set up

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = (3000) ;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(port, () => console.log(`app listening at http://localhost:${port}`));
