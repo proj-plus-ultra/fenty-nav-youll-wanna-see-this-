314f58c25656ea8d942f51f9828952aa25ac1a50
Alex Benko <benkoalex521@gmail.com>
2020-04-30 15:52:35 -0700

    Moved the youtube request here since its not a component

---
+++ client/src/scripts/getVideos.js
@@ -0,0 +1,26 @@
+import react from 'react';
+import axios from 'axios';
+
+const searchYT = ({key, productName}, callback) =>{
+  console.log(key);
+  console.log(productName);
+  let paramsObj = {
+    part: 'snippet',
+    channelId: 'UC0HVR9T6oFS3veefhGCGEsw',
+    key: key,
+    q: productName,
+    maxResults: 5,
+    type: 'video',
+    videoEmbeddable: true
+  };
+
+  axios.get('https://www.googleapis.com/youtube/v3/search', {params: paramsObj})
+    .then((res) => {
+      console.log('Retrieved Videos Succesfully!', res);
+      callback(res);
+    })
+    .catch((err) => console.error(err));
+};
+
+
+export default searchYT;
