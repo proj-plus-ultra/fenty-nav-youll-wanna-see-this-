3fe83a61f96baeb71baf5066e79c0a9e36738c5a
Alex Benko <benkoalex521@gmail.com>
2020-04-30 15:36:32 -0700

    Imported components and set up funtion to get videos

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,22 +1,60 @@
 import React from 'react';
+import getVideos from './getVideos.jsx';
+import axios from 'axios';
+import Videos from './Videos.jsx';
+import Footer from './Footer.jsx';
 
-class App extends React.Component{
-  constructor(props){
-    super(props)
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
     this.state = {
-      productName : ''
-    }
+      products: [],
+      viedos: [],
+      current: ''
+    };
   }
 
+  getProducts() {
+    axios.get('/products')
+      .then((res) =>{
+        console.log('Got Products!', res.data);
+        this.setState({
+          products: res.data
+        });
+      })
+      .catch((err) => console.error(err));
+  }
 
-  render(){
-    return(
-      <div>
-        test
+  getYouTubeVideos(name) {
+    let key = props.API_key;
+    let toGet = this.state.current;
+    getVideos({key, toGet}, (err, results) =>{
+      if (err) {
+        console.error('Error Getting Videos:', err);
+      } else {
+        this.setState({
+          vidoes: results
+        }, () => console.log('Retrieved Videos!', this.state.vidoes));
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  render() {
+    return (
+      <div className = "body">
+        <Videos />
+        <Footer />
       </div>
     );
   }
+
+
 }
 
 export default App;
