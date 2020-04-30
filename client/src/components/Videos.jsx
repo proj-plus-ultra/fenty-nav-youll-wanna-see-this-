9a8ffdfd2df6cdbebb001b1b75b1e5dffcf467d7
Alex Benko <benkoalex521@gmail.com>
2020-04-30 15:32:01 -0700

    The you'll wanna see this component

---
+++ client/src/components/Videos.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Videos extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+
+    return (
+      <div className="youll-wanna-see-this">
+        bop
+      </div>
+    );
+
+  }
+}
+
+export default Videos;
