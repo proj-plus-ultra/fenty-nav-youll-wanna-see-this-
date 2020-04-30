734a3980f2aebbdb7cbed18b8e13718c359979c7
Alex Benko <benkoalex521@gmail.com>
2020-04-30 10:36:38 -0700

    Fixed eslint errors and set up endpoint to retireve all info from the database

--- server/index.js
+++ server/index.js
@@ -4,14 +4,26 @@ const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 const db = require('../database/index.js');
+const dbHelpers = require('../database/dbHelpers.js');
 
 const app = express();
-const port = (3000) ;
+const port = (3000);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 app.use(morgan('dev'));
+
+//END POINTS//
+app.use('/products', (req, res) =>{
+  dbHelpers.getProducts((err, results) =>{
+    if (err) {
+      res.status(400).json(err);
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
 
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
