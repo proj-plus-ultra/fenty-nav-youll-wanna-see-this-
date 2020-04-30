cc8607ba7c18bf46106612f5a6bbfce2c15e9cc8
Alex Benko <benkoalex521@gmail.com>
2020-04-30 10:36:57 -0700

    Fixed eslint errors

--- foo.js
+++ foo.js
@@ -3,29 +3,29 @@ let colors = document.getElementsByClassName('hexa');
 
 let results = [];
 
-for(let i = 0; i < colors.length;i++){
-    let hex = colors[i].style.backgroundColor;
-    results.push(hex);
+for (let i = 0; i < colors.length; i++) {
+  let hex = colors[i].style.backgroundColor;
+  results.push(hex);
 }
 
-let foo = []
+let foo = [];
 
 //gets rid of rgb and makes it just the number
 results.map((value) =>{
-  let temp = value.split("(")[1].split(")")[0];
+  let temp = value.split('(')[1].split(')')[0];
 
-  temp = temp.split(",");
+  temp = temp.split(',');
 
   foo.push(temp);
-})
+});
 
 let foobar = [];
 
-for(let i = 0; i < foo.length;i++){
+for (let i = 0; i < foo.length; i++) {
   let bar = foo[i].map((value)=>{
     value = parseInt(value).toString(16);
-    return (value.length === 1) ? "0"+ value : value;
-  })
+    return (value.length === 1) ? "0" + value : value;
+  });
   foobar.push("#" + bar.join(""));
 }
 
