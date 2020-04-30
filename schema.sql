8894ff30285882e8c47801f9cd5a04f2ca8f7dbb
Alex Benko <benkoalex521@gmail.com>
2020-04-29 20:46:36 -0700

    Schema for database

---
+++ schema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS fenty;
+
+CREATE DATABASE fenty;
+
+USE fenty;
+
+CREATE TABLE IF NOT EXISTS products (
+  id INT AUTO_INCREMENT primary key,
+  name VARCHAR(250) NOT NULL,
+  price INT NOT NULL,
+  color VARCHAR(300) NOT NULL,
+  category VARCHAR(250) NOT NULL,
+  descrip VARCHAR(1000) NOT NUll,
+  rating INT NOT NULL,
+  background VARCHAR(1000) NOT NULL,
+  foreground VARCHAR(1000) NOT NULL,
+  bg_color VARCHAR(25) NOT NULL
+
+
+);
