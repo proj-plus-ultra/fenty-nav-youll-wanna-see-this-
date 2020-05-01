188e1b0161e5626f52e4cc295ae46cb9ea4455ec
Alex Benko <benkoalex521@gmail.com>
2020-05-01 16:35:45 -0700

    Added the play button image source

--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+const buttonSource = 'https://www.fentybeauty.com/on/demandware.static/-/Sites-FENTY-Library/default/dwbba3103b/homepage/playbutton_mobile.svg';
+
 
 const Video = ({video}) =>(
 
-    <div className='entry'>
-
-      <img src={video.snippet.thumbnails.medium.url}></img>
+    <div className='entry' >
+      <img src = {buttonSource} className='buttonPlay' ></img>
+      <img src={video.snippet.thumbnails.medium.url} className='thumbnail'></img>
       <div className='subtitle'>
          {video.snippet.title}
       </div>
