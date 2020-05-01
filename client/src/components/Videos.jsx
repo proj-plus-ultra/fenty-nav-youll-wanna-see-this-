580bf573d6dd83d1aeba3d9e6bc24ef417f28afa
Alex Benko <benkoalex521@gmail.com>
2020-05-01 12:41:59 -0700

    Passing in video data to Video component

--- client/src/components/Videos.jsx
+++ client/src/components/Videos.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
+import Video from './Video.jsx'
 
 const Videos = ({videos}) =>{
   return (
     <div className="youll-wanna-see-this">
       <h3 className='content-header'>YOU'LL WANNA SEE THIS</h3>
       {console.log('From component', videos)}
-      {videos.map((video, i) =>{
-        return(
-        <h3 key={i}>{video.snippet.title}</h3>
-        )
-      })}
+      {videos.map((video,i) => <Video key={i} video={video}/>)}
     </div>
   );
 };
