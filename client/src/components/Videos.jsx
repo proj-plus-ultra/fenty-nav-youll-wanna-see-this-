2ffb68f05df2bcddb9a5f487d2a94a3de2a026e4
Alex Benko <benkoalex521@gmail.com>
2020-05-01 11:55:18 -0700

    Renders video info to page

--- client/src/components/Videos.jsx
+++ client/src/components/Videos.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 const Videos = ({videos}) =>{
   return (
     <div className="youll-wanna-see-this">
+      <h3 className='content-header'>YOU'LL WANNA SEE THIS</h3>
       {console.log('From component', videos)}
-      bop
+      {videos.map((video, i) =>{
+        return(
+        <h3 key={i}>{video.snippet.title}</h3>
+        )
+      })}
     </div>
   );
 };
