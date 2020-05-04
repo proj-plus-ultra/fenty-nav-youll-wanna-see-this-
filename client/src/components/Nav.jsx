882a1df029e9f91e6af29bb0c55210fe64e179ed
Alex Benko <benkoalex521@gmail.com>
2020-05-04 12:49:08 -0700

    File for Nav bar

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <header>
+        <div className='navContainer'>
+          <nav className='navbar'>
+            bop
+          </nav>
+        </div>
+      </header>
+    );
+  }
+
+}
+
+export default Nav;
