e195e99a89cba0984a82ce52a58a26934a2ebb3a
Alex Benko <benkoalex521@gmail.com>
2020-05-03 18:00:13 -0700

    Clicking on any video now opens a popup with itd embeded video

--- client/src/components/Popup.jsx
+++ client/src/components/Popup.jsx
@@ -7,30 +7,16 @@ const buttonSource = 'https://www.fentybeauty.com/on/demandware.static/-/Sites-F
 class VideoPopup extends React.Component{
   constructor(props){
     super(props);
-    this.state = { open: false };
-    this.openModal = this.openModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
+
   }
-
-  openModal() {
-    this.setState({ open: true });
-  }
-  closeModal() {
-    this.setState({ open: false });
-  }
-
-
-
   render(){
     return (
       <div>
-         <button className="button" onClick={this.openModal}>
-          Controlled Popup
-        </button>
 
-        <Popup open={this.state.open} closeOnDocumentClick onClose={this.closeModal}>
+
+        <Popup open={this.props.isOpen} closeOnDocumentClick onClose={this.closeModal}>
           <div className="modal">
-            <iframe frameborder="0"src={`https://www.youtube.com/embed/${this.props.vidId}`} width="640" height="360"></iframe>
+            <iframe frameBorder="0"src={`https://www.youtube.com/embed/${this.props.vidId}`} width="640" height="360"></iframe>
             {this.props.title}
           </div>
         </Popup>
