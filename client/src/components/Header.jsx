cc9f661db264ebd599f9bff0f5c5d81396fcf2b3
Alex Benko <benkoalex521@gmail.com>
2020-05-04 13:29:38 -0700

    Header has fenty logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 class Header extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      navItems: ['SHOP', 'SALE', 'SHADE FINDER', 'RIHANNA', 'CLARA LIONEL FOUNDATION']
+    };
   }
 
 
@@ -10,9 +13,14 @@ class Header extends React.Component {
     return (
       <header>
         <div className='headerContainer'>
+          <div className='logo'>
+            {/*TODO: Change src to whatever/wherever the homepage is*/}
+            <a src='#'/>
+            <img src="https://i.ibb.co/1RyTT3n/fentybeauty-logo.png" alt="fentybeauty-logo" border="0" className='logo-img'/>
+          </div>
           <div className='navContainer'>
             <nav className='navbar'>
-            bop
+
             </nav>
           </div>
         </div>
