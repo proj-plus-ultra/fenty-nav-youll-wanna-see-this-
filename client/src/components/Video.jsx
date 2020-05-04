5d233feb4e6146f9b763afa85102f7f84f4bc9fb
Alex Benko <benkoalex521@gmail.com>
2020-05-03 17:59:48 -0700

    Popup functions and state now in Video and passes them down instead

--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -8,7 +8,7 @@ const buttonSource = 'https://www.fentybeauty.com/on/demandware.static/-/Sites-F
 class Video extends React.Component{
   constructor(props){
     super(props);
-
+    this.state = { open: false };
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
 
@@ -25,10 +25,10 @@ class Video extends React.Component{
   render(){
     return (
 
-    <div className='entry' >
-      {console.log(this.props.video.id.videoId)}
-      <VideoPopup title={this.props.video.snippet.title} vidId = {this.props.video.id.videoId} />
+    <div className='entry' onClick={this.openModal}>
 
+      <VideoPopup title={this.props.video.snippet.title} vidId = {this.props.video.id.videoId} isOpen={this.state.open} open={this.openModal} close={this.closeModal}/>
+      <img src={buttonSource} className='buttonPlay'></img>
       <img src={this.props.video.snippet.thumbnails.medium.url} className='thumbnail'></img>
       <div className='subtitle'>
          {this.props.video.snippet.title}
