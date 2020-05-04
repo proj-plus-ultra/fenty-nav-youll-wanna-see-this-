5294300a0069900f8cbab15477376330a3538f26
Alex Benko <benkoalex521@gmail.com>
2020-05-04 12:56:56 -0700

    Renamed to header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <header>
+        <div className='headerContainer'>
+          <div className='navContainer'>
+            <nav className='navbar'>
+            bop
+            </nav>
+          </div>
+        </div>
+      </header>
+    );
+  }
+
+}
+
+export default Header;
