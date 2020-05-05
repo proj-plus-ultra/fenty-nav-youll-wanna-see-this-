f392c259e3aceaede13c8cc06db6b60024a87d5f
Alex Benko <benkoalex521@gmail.com>
2020-05-05 15:23:59 -0700

    Added the icons on the right side of the header

--- client/src/components/RightHeader/RightHeader.jsx
+++ client/src/components/RightHeader/RightHeader.jsx
@@ -12,9 +12,9 @@ class RightHeader extends React.Component {
     return (
       <div className='rightHeader'>
         <ul className='rightList'>
-          <li><p>Search</p></li>
-          <li> <img src="https://i.ibb.co/0FWLjfJ/login-icon.png" alt="login-icon" border="0" className='actImg'/></li>
-          <li></li>
+          <li><img src="https://i.ibb.co/2tBFgVx/Screen-Shot-2020-05-05-at-3-07-23-PM.png" border="0" className='search'/></li>
+          <li><img src="https://i.ibb.co/0FWLjfJ/login-icon.png" alt="login-icon" border="0" className='actImg'/></li>
+          <li><img src="https://i.ibb.co/v1zZB92/Screen-Shot-2020-05-05-at-3-16-41-PM.png" alt="shopping-bag-image" border="0" className='bag'/></li>
         </ul>
       </div>
     );
