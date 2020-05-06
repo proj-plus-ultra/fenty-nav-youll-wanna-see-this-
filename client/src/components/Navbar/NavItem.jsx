04cf66a7383de42a99b060ff94b0b6a994eb2db3
Alex Benko <benkoalex521@gmail.com>
2020-05-06 15:53:11 -0700

    Direcotry for all files realated to navbar/header

---
+++ client/src/components/Navbar/NavItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class NavItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <li className='navListItem'>
+        {this.props.item}
+      </li>
+    );
+  }
+
+}
+
+export default NavItem;
