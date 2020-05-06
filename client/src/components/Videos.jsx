6d4d7ef32346f19b16744f4875f0d400d39912ad
Alex Benko <benkoalex521@gmail.com>
2020-05-06 16:24:51 -0700

    Fixed spacing

--- client/src/components/Videos.jsx
+++ client/src/components/Videos.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Video from './Video.jsx'
+import Video from './Video.jsx';
 
 const Videos = ({videos}) =>{
   return (
@@ -7,7 +7,7 @@ const Videos = ({videos}) =>{
       <h3 className='content-header'>YOU'LL WANNA SEE THIS</h3>
       {console.log('From component', videos)}
       <div className='flexbox'>
-      {videos.map((video,i) => <Video key={i} video={video}/>)}
+        {videos.map((video, i) => <Video key={i} video={video}/>)}
       </div>
     </div>
   );
