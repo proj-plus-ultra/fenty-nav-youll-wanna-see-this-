9be0c260ef19acecd913c8ef48d6d984ab53fba7
Alex Benko <benkoalex521@gmail.com>
2020-05-06 16:24:35 -0700

    The search bar

--- client/src/components/Navbar/Search.jsx
+++ client/src/components/Navbar/Search.jsx
@@ -3,13 +3,34 @@ import React from 'react';
 class Search extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      query: 'What Are You Looking For?',
+      products: []
+    };
+
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch(e) {
+    console.log(e.target.value);
+    this.setState({
+      query: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    //this will query the database based on what is typed
+    //search results show up as sooon as 1 character is entered
   }
 
 
   render() {
     return (
-      <div>
-        bop
+      <div className='searchBar'>
+
+        <input type='text' value={this.state.query} onChange={this.handleSearch} className='searchBox'></input>
+
       </div>
     );
   }
