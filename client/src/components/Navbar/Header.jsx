23b6e046fe8e953db61c130b927e7da0124c0290
Alex Benko <benkoalex521@gmail.com>
2020-05-06 16:22:54 -0700

    Conditional rendering for the search bar and related functions

--- client/src/components/Navbar/Header.jsx
+++ client/src/components/Navbar/Header.jsx
@@ -7,13 +7,15 @@ class Header extends React.Component {
     super(props);
 
     this.state = {
-      showSearchBar: true
+      showSearchBar: false
     };
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
+    console.log('Clicked!');
+
     this.setState({
       showSearchBar: !this.state.showSearchBar
     });
@@ -29,7 +31,7 @@ class Header extends React.Component {
               <a src='#'/>
               <img src="https://i.ibb.co/1RyTT3n/fentybeauty-logo.png" alt="fentybeauty-logo" border="0" className='logo-img'/>
             </div>
-            <Nav />
+            <Nav showSearch={this.handleClick}/>
             <Search />
           </div>
         </header>
@@ -43,7 +45,7 @@ class Header extends React.Component {
               <a src='#'/>
               <img src="https://i.ibb.co/1RyTT3n/fentybeauty-logo.png" alt="fentybeauty-logo" border="0" className='logo-img'/>
             </div>
-            <Nav />
+            <Nav showSearch={this.handleClick}/>
           </div>
         </header>
       );
