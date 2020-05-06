440bb1890d575a5e8405471a69db3a1600746396
Alex Benko <benkoalex521@gmail.com>
2020-05-06 16:24:12 -0700

    Added onclick to the search icon so when its clicked, it will render the search bar

--- client/src/components/Navbar/RightHeader.jsx
+++ client/src/components/Navbar/RightHeader.jsx
@@ -14,7 +14,7 @@ class RightHeader extends React.Component {
     return (
       <div className='rightHeader'>
         <ul className='rightList'>
-          <li><img src="https://i.ibb.co/2tBFgVx/Screen-Shot-2020-05-05-at-3-07-23-PM.png" border="0" className='search'/></li>
+          <li onClick={this.props.showSearch}><img src="https://i.ibb.co/2tBFgVx/Screen-Shot-2020-05-05-at-3-07-23-PM.png" border="0" className='search'/></li>
           <li><img src="https://i.ibb.co/0FWLjfJ/login-icon.png" alt="login-icon" border="0" className='actImg'/></li>
           <li><img src="https://i.ibb.co/v1zZB92/Screen-Shot-2020-05-05-at-3-16-41-PM.png" alt="shopping-bag-image" border="0" className='bag'/></li>
         </ul>
