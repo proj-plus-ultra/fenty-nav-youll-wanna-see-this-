84d8bc5725d2c33db55575e68249a7ace17fb467
Alex Benko <benkoalex521@gmail.com>
2020-05-09 12:51:24 -0700

    Put search results into table instead of list

--- client/src/components/Navbar/SearchResults.jsx
+++ client/src/components/Navbar/SearchResults.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 const SearchResults = (props) =>{
+  let shortened = props.products.slice(0, 8);
+  let rowOne = shortened.slice(0, 4);
+  let rowTwo = shortened.slice(4);
+
   return (
-    <li>
-      <div className='searchResults'>
-        <img src={props.product.foreground}/>
-        {props.product.name}
-        <br/>
-        {props.product.price}
-      </div>
-    </li>
+    <table className='searchList'>
+      <tr>
+        {rowOne.map((product, i) =>{
+          return (
+            <td key={i}>
+              <img src={product.foreground}/>
+              {product.name}
+              ${product.price}
+            </td>
+          );
+        })}
+      </tr>
+
+      <tr>
+        {rowTwo.map((product, i) =>{
+          return (
+            <td key={i}>
+              <img src={product.foreground}/>
+              {product.name}
+              ${product.price}
+            </td>
+          );
+        })}
+      </tr>
+    </table>
+
   );
 
 };
