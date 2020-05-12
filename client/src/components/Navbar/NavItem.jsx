139434a789554136ba95cf4b4b3e1e263271830b
Alex Benko <benkoalex521@gmail.com>
2020-05-11 18:35:07 -0700

    Drop down appears when SHOP is hovered over

--- client/src/components/Navbar/NavItem.jsx
+++ client/src/components/Navbar/NavItem.jsx
@@ -4,58 +4,21 @@ class NavItem extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      showShop: false,
-      showRiRi: false
-    };
+
   }
 
   render() {
 
-    if (this.state.showShop) {
-      return (
-        <div>
-          <div>
-            <li className='navListItem'>SHOP</li>
-            <li className='navListItem'>SALE</li>
-            <li className='navListItem'>SHADE FINDER</li>
-            <li className='navListItem'>RIHANNA</li>
-            <li className='navListItem'>CLARA LIONEL FOUNDATION</li>
-          </div>
+    return (
+      <div>
+        <li className='navListItem' onMouseEnter={this.props.handleShopHover} onMouseLeave={this.props.handleShopHover}>SHOP</li>
+        <li className='navListItem'>SALE</li>
+        <li className='navListItem'>SHADE FINDER</li>
+        <li className='navListItem'>RIHANNA</li>
+        <li className='navListItem'>CLARA LIONEL FOUNDATION</li>
+      </div>
+    );
 
-          <div className='riDropDown'>
-          testy shop
-          </div>
-        </div>
-      );
-
-    } else if (this.state.showRiRi) {
-      return (
-        <div>
-          <div>
-            <li className='navListItem'>SHOP</li>
-            <li className='navListItem'>SALE</li>
-            <li className='navListItem'>SHADE FINDER</li>
-            <li className='navListItem'>RIHANNA</li>
-            <li className='navListItem'>CLARA LIONEL FOUNDATION</li>
-          </div>
-
-          <div className='riDropDown'>
-          testy
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <li className='navListItem'>SHOP</li>
-          <li className='navListItem'>SALE</li>
-          <li className='navListItem'>SHADE FINDER</li>
-          <li className='navListItem'>RIHANNA</li>
-          <li className='navListItem'>CLARA LIONEL FOUNDATION</li>
-        </div>
-      );
-    }
   }
 
 }
