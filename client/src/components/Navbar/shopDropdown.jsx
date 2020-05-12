bcb493f2b6a518d1a221e8ce7dd3e1acd3af8ec7
Alex Benko <benkoalex521@gmail.com>
2020-05-11 20:50:28 -0700

    Just need to render the rest of the lists

--- client/src/components/Navbar/shopDropdown.jsx
+++ client/src/components/Navbar/shopDropdown.jsx
@@ -7,20 +7,46 @@ class shopDropdown extends React.Component {
 
   render() {
     let shop1Style = {
-      display: this.props.isShow ? 'block' : 'none'
+      display: this.props.isShow ? 'block' : 'none',
+      padding: '30px 20px 25px 10px',
+      letterSpacing: '2px',
+      fontSize: '13px',
+      fontWeight: 'bold'
     };
 
     let shop1 = ['SHOP ALL', 'SHOW NEW', 'BEST SELLERS', 'NEW! CHEEKS OUT COLLECTION', 'COLLECTIONS', 'SALE', 'FOUNDATION SHADE FINDER', 'E-GIFT CARDS'];
+
+    let face = ['Foundation', 'Prime + Set', 'Concealer', 'Powder', 'Contour', 'Blotting'];
+    let cheek = ['Blush', 'Bronzer', 'Highlighter'];
+    let lip = ['Lip Gloss', 'Lipstick', 'Lip Care'];
+    let eye = [ 'BROW', 'BODY'];
+    let misc = ['MINIS + Travel Size', 'VALUE SETS + VAULTS', 'BRUSHES + TOOLS', 'ACCESSORIES', 'EXCLUSIVES', "RIHANNA's FAVES"];
     return (
 
-      <div className='showShopDD' style={shop1Style}>
-        <ul className='shopDD1'>
+      <div className='showShopDD' style={shop1Style} onMouseLeave={this.props.close}>
+        <ul className='shopDD1' style={{paddingLeft: '300px', textAlign: 'left'}}>
           {shop1.map((item, i) =>{
             return (
               <li className='dd1' key={i}>{item}</li>
             );
           })}
         </ul>
+
+        <div className='faceDDD' style={{position: 'relative', textAlign: 'center', top: '0'}}>
+          <ul className='faceDD' >
+            <li>
+              <h3>FACE</h3>
+              <ul>
+                {face.map((item, i) =>{
+                  return (
+                    <li className='faceElem' key={i} style={{fontSize: '10px'}}>{item}</li>
+                  );
+                })}
+              </ul>
+            </li>
+          </ul>
+        </div>
+
       </div>
 
     );
