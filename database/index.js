80b448f70e6bc9e5d7118db4cabf6012bf91bc50
Alex Benko <benkoalex521@gmail.com>
2020-05-15 10:55:40 -0700

    Minor changes, connects to aws hosted server

--- database/index.js
+++ database/index.js
@@ -13,12 +13,13 @@ const db = mysql.createConnection({
 
 db.connect((err)=>{
   if (err) {
-    console.log('Error connecting to Database:', err);
+    console.log('Error connecting to Database:', err.sqlMessage);
   }
 
   db.query('USE fenty_nav_videos');
 
   console.log('Connected to database fenty_nav_videos successfully');
 });
+
 
 module.exports = db;
