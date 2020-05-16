5db3f3813c03a96d1704ec19ae792e7169dc79a9
Alex Benko <benkoalex521@gmail.com>
2020-05-16 10:28:29 -0700

    Changed schema so it only help data my component needed

--- schema.sql
+++ schema.sql
@@ -8,13 +8,5 @@ CREATE TABLE IF NOT EXISTS products (
   id INT AUTO_INCREMENT primary key,
   name VARCHAR(250) NOT NULL,
   price INT NOT NULL,
-  color VARCHAR(300) NOT NULL,
-  category VARCHAR(250) NOT NULL,
-  descrip VARCHAR(1000) NOT NUll,
-  rating INT NOT NULL,
-  background VARCHAR(1000) NOT NULL,
-  foreground VARCHAR(1000) NOT NULL,
-  bg_color VARCHAR(25) NOT NULL
-
-
+  foreground VARCHAR(1000) NOT NULL
 );
